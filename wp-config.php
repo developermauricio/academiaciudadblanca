5450a37a19142e2c58006390467c064c6c40584f
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-04-10 14:11:14 +0000

    ajustes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'acciudadablanca' );
+define( 'DB_NAME', 'acciudadblanca' );
 
 /** Database username */
 define( 'DB_USER', 'forge' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Q2b2TQEDvfeD0jiIyF6R' );
+define( 'DB_PASSWORD', 'fR7ZbKrA0ULfnIR8kslk' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
